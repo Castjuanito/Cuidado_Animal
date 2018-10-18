6d92e5137128a6c5a96278667ca3189ba4c7dade
Juan Castañeda <juanario1997@gmail.com>
2018-10-18 01:02:47 -0500

    tablas dueño de veterinaria, vetrinaria , servicio, palabra clave

--- CuidadoAnimal/models.py
+++ CuidadoAnimal/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class CentroVeterinario(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=30)
+    horaA = models.DateTimeField()
+    horaC = models.DateField()
+
+
+class Servicio(models.Model):
+
+    detalle = models.CharField(max_length=30)
+    centroVeterinario = models.ForeignKey(CentroVeterinario, on_delete=models.CASCADE)
+
+
+class PalabraClave(models.Model):
+
+    servicio = models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    palabra  = models.CharField(max_length=30)
+
+
+class AdminostradorCentroVeterinario(models.Model):
+
+    nombreUsuario = models.CharField(max_length=30)
+    password = models.CharField(max_length=1000)
+    centroVeterinario = models.OneToOneField(
+        CentroVeterinario,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
