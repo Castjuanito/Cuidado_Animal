f8f7418334ed17c1295adcb4f4ff0e232e1d2070
Juan Castañeda <juanario1997@gmail.com>
2018-10-18 01:16:15 -0500

    Merge branch 'master' of https://github.com/Castjuanito/Cuidado_Animal into JuanitoBranch
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	CuidadoAnimal/models.py

--- CuidadoAnimal/models.py
+++ CuidadoAnimal/models.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.db import models
 
 
@@ -30,13 +32,15 @@ class AdminostradorCentroVeterinario(models.Model):
         on_delete=models.CASCADE,
         primary_key=True,
     )
+
+
 # Create your models here.
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     cedula = models.IntegerField(max_length=12)
-    password: str = models.CharField(max_length=1000)
+    password = models.CharField(max_length=1000)  # type: Any
 
 
 class MedicoVeterinario(Usuario):
