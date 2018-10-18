6e0c7603abfb92f86cf021f8bc2a46c187d3e423
4Khaos <imroocker@gmail.com>
2018-10-18 05:09:12 -0500

    classes

--- CuidadoAnimal/models.py
+++ CuidadoAnimal/models.py
@@ -6,4 +6,29 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=20)
     apellido = models.CharField(max_length=20)
     cedula = models.IntegerField(max_length=12)
-    password = models.CharField(max_length=1000)
+    password: str = models.CharField(max_length=1000)
+
+
+class MedicoVeterinario(Usuario):
+    especialidad = models.CharField(max_length=20)
+
+
+class DuenoMascota(Usuario):
+    direccion = models.CharField(max_length=30)
+    telefono = models.IntegerField(max_length=12)
+    correo = models.CharField(max_length=30)
+
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=20)
+    especie = models.CharField(max_length=20)
+    raza = models.CharField(max_length=20)
+    fechaNacimiento = models.DateField()
+    GENEROS = (
+        ('H', 'Hombre'),
+        ('M', 'Mujer'),
+        ('O', 'Otro'),
+    )
+    genero = models.CharField(max_length=1, choices=GENEROS)
+    color = models.CharField(max_length=10)
+    edad = models.IntegerField(max_length=3)
