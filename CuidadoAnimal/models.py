c65650ad0ea7e27897d61b4adf5a4d9a0f255f12
4Khaos <imroocker@gmail.com>
2018-10-18 04:39:17 -0500

    Update readme !

--- CuidadoAnimal/models.py
+++ CuidadoAnimal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    cedula = models.IntegerField(max_length=12)
+    password = models.CharField(max_length=1000)
